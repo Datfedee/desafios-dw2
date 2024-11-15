467125bb26b33db4ee2e05744c8062635a1b40c0
Datfedee <datfedee@gmail.com>
2024-11-15 18:16:29 -0300

    desafío 7

---
+++ desafio_7/desafío7.js
@@ -0,0 +1,7 @@
+let precioproducto = document.getElementById ("precioproductos")
+
+precioproducto.innerText = "precio: $450"
+
+let nombreproducto = document.querySelector (".nombreproducto")
+
+nombreproducto.style.color = "green"
